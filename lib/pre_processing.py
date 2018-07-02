d1479558e4cb8028b6df8929102c2adc92a65916
huckiyang <huckiyang@gmail.com>
2018-07-02 17:53:42 +0200

    Add files via upload

---
+++ lib/pre_processing.py
@@ -0,0 +1,76 @@
+###################################################
+#
+#   Script to pre-process the original imgs
+#
+##################################################
+
+
+import numpy as np
+from PIL import Image
+import cv2
+
+from help_functions import *
+
+
+#My pre processing (use for both training and testing!)
+def my_PreProc(data):
+    assert(len(data.shape)==4)
+    assert (data.shape[1]==3)  #Use the original images
+    #black-white conversion
+    train_imgs = rgb2gray(data)
+    #my preprocessing:
+    train_imgs = dataset_normalized(train_imgs)
+    train_imgs = clahe_equalized(train_imgs)
+    train_imgs = adjust_gamma(train_imgs, 1.2)
+    train_imgs = train_imgs/255.  #reduce to 0-1 range
+    return train_imgs
+
+#========= PRE PROCESSING FUNCTIONS ========================#
+
+#==== histogram equalization
+def histo_equalized(imgs):
+    assert (len(imgs.shape)==4)  #4D arrays
+    assert (imgs.shape[1]==1)  #check the channel is 1
+    imgs_equalized = np.empty(imgs.shape)
+    for i in range(imgs.shape[0]):
+        imgs_equalized[i,0] = cv2.equalizeHist(np.array(imgs[i,0], dtype = np.uint8))
+    return imgs_equalized
+
+
+# CLAHE (Contrast Limited Adaptive Histogram Equalization)
+def clahe_equalized(imgs):
+    assert (len(imgs.shape)==4)  #4D arrays
+    assert (imgs.shape[1]==1)  #check the channel is 1
+    #create a CLAHE object (Arguments are optional).
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+    imgs_equalized = np.empty(imgs.shape)
+    for i in range(imgs.shape[0]):
+        imgs_equalized[i,0] = clahe.apply(np.array(imgs[i,0], dtype = np.uint8))
+    return imgs_equalized
+
+
+# ===== normalize over the dataset
+def dataset_normalized(imgs):
+    assert (len(imgs.shape)==4)  #4D arrays
+    assert (imgs.shape[1]==1)  #check the channel is 1
+    imgs_normalized = np.empty(imgs.shape)
+    imgs_std = np.std(imgs)
+    imgs_mean = np.mean(imgs)
+    imgs_normalized = (imgs-imgs_mean)/imgs_std
+    for i in range(imgs.shape[0]):
+        imgs_normalized[i] = ((imgs_normalized[i] - np.min(imgs_normalized[i])) / (np.max(imgs_normalized[i])-np.min(imgs_normalized[i])))*255
+    return imgs_normalized
+
+
+def adjust_gamma(imgs, gamma=1.0):
+    assert (len(imgs.shape)==4)  #4D arrays
+    assert (imgs.shape[1]==1)  #check the channel is 1
+    # build a lookup table mapping the pixel values [0, 255] to
+    # their adjusted gamma values
+    invGamma = 1.0 / gamma
+    table = np.array([((i / 255.0) ** invGamma) * 255 for i in np.arange(0, 256)]).astype("uint8")
+    # apply gamma correction using the lookup table
+    new_imgs = np.empty(imgs.shape)
+    for i in range(imgs.shape[0]):
+        new_imgs[i,0] = cv2.LUT(np.array(imgs[i,0], dtype = np.uint8), table)
+    return new_imgs
